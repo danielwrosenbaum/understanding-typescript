54a7886af74054eff659de2974d6cbe60e92604c
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-07-27 10:43:31 -0700

    added enums exercise.

--- course-starting-project 2/app.ts
+++ course-starting-project 2/app.ts
@@ -1,13 +1,25 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // tuple //
+// } = {
+//   name: 'Daniel',
+//   age: 35,
+//   hobbies: ['Hiking', 'Reading'],
+//   role: [2, 'author'] // tuple = fixed array: role must have two indexes with a number followed by a string //
+// };
+// const ADMIN = 0;
+// const  READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 'ADMIN', READ_ONLY = 100, AUTHOR = 200};
+
+const person = {
   name: 'Daniel',
   age: 35,
   hobbies: ['Hiking', 'Reading'],
-  role: [2, 'author']
+  role: Role.ADMIN
 };
 
 let favoriteActivites: string[];
@@ -17,4 +29,8 @@ console.log(person.name)
 
 for (const hobby of person.hobbies) {
   console.log(hobby)
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('is admin');
 }
