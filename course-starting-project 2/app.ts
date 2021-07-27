c8521c96fd27699aed0d1a458801f63eea488104
Daniel Rosenbaum <danwrosenbaum@gmail.com>
2021-07-27 10:26:19 -0700

    added array exercise

--- course-starting-project 2/app.ts
+++ course-starting-project 2/app.ts
@@ -1,8 +1,18 @@
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
 } = {
   name: 'Daniel',
-  age: 35
+  age: 35,
+  hobbies: ['Hiking', 'Reading']
 };
+
+let favoriteActivites: string[];
+favoriteActivites = ['Sports'];
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby)
+}
